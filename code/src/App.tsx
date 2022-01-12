237e3fa68f8f2e9354bb9965b67b52bd1aa726e8
Anna <anna.almestal@tretton37.com>
2022-01-12 16:26:55 +0100

    Configured TS and changed file  structure

---
+++ code/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { VoidFunctionComponent } from "react";
+
+const App = () => {
+  const message: string = "Hello World 🎉";
+  return (
+    <>
+      <h1>{message}</h1>
+    </>
+  );
+};
+
+export default App;
